b2172af740e845bbb66e2f2f49230c644a8daac6
Carl Suster <Carl.Suster@health.nsw.gov.au>
2023-04-19 13:59:29 +1000

    Register maps from maps package

--- R/zzz.R
+++ R/zzz.R
@@ -2,4 +2,28 @@
   register_map("sf.nc",
     function() sf::st_read(system.file("shape/nc.shp", package = "sf"), quiet = TRUE),
     feature_column = "NAME")
+
+  register_map("maps.world",
+    function() sf::st_as_sf(maps::map('world', plot = FALSE, fill = TRUE)),
+    feature_column = "ID")
+
+  register_map("maps.italy",
+    function() sf::st_as_sf(maps::map('italy', plot = FALSE, fill = TRUE)),
+    feature_column = "ID")
+
+  register_map("maps.france",
+    function() sf::st_as_sf(maps::map('france', plot = FALSE, fill = TRUE)),
+    feature_column = "ID")
+
+  register_map("maps.nz",
+    function() sf::st_as_sf(maps::map('nz', plot = FALSE, fill = TRUE)),
+    feature_column = "ID")
+
+  register_map("maps.state",
+    function() sf::st_as_sf(maps::map('state', plot = FALSE, fill = TRUE)),
+    feature_column = "ID")
+
+  register_map("maps.lakes",
+    function() sf::st_as_sf(maps::map('lakes', plot = FALSE, fill = TRUE)),
+    feature_column = "ID")
 }
